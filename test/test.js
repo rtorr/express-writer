cc242ca1843c41ff35bc02ebfb3e413973a7ff2c
rtorr <rtorruellas@gmail.com>
2013-11-15 14:48:10 -0700

    leaving a future linting comment

--- test/test.js
+++ test/test.js
@@ -4,32 +4,30 @@ var rimraf = require("rimraf");
 var writer = require('../index');
 var assert = require('assert');
 
+/* mocha globals - describe, beforeEach, afterEach, it */
 
 describe('Write something', function(){
-  describe('Blah', function(){
 
+  describe('Blah', function(){
     beforeEach(function(done){
       writer.scribe(req, '<h1>Hello World</h1>', done);
     });
-
     afterEach(function(done){
       rimraf(path.join(__dirname, "../dist"), function (er) {
         if (er) throw er;
         done();
       })
     });
-
     var req = {
       originalUrl: '/hello-world'
     };
-
     it('It should write something to dist', function(done){
       fs.readFile('dist/hello-world/index.html', 'utf8', function(err,data){
         assert(data === '<h1>Hello World</h1>');
         done();
       });
     });
+  });
 
-  })
 });
 
